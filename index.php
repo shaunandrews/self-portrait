fc477c75a9bbd10675ff1163994754fa45ecc233
Shaun Andrews <shaun@automattic.com>
2020-09-30 15:36:39 -0400

    Copying over the bare-bones, mostly from Seedlet.

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+  get_header();
+?>
+
+  <section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+    /*
+		if ( have_posts() ) {
+
+			// Load posts loop.
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content/content' );
+			}
+
+			// Previous/next page navigation.
+			seedlet_the_posts_navigation();
+
+		} else {
+
+			// If no content, include the "No posts found" template.
+			get_template_part( 'template-parts/content/content-none' );
+
+		}
+    */
+		?>
+
+		</main>
+	</section>
+
+<?php
+  get_footer();
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== shaunandrews ===
+Contributors: Shaun Andrews
+Requires at least: 5.0
+Tested up to: 5.4.1
+Requires PHP: 7.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+I've no clue.
+
+== Description ==
+
+Fuck if I know.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+== Copyright ==
+
+I couldn't care less.
